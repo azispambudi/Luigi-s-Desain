b703d4d9eb39c5c79d16d229b08068974e4fdf5e
Azis Pambudi <azispampam@gmail.com>
2021-01-05 19:54:34 +0700

    komit pertama

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Luigi's Design</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  
+  <!-- bagian header template -->
+  <header>
+    <h1 class="judul">Luigi's Design</h1>
+    <p class="deskripsi">Menyediakan jasa desain Kalender, Poster, Pamflet, Dll.</p>
+  </header>
+  <!-- akhir bagian header template -->
+  
+  <div class="wrap">
+    <!-- bagian menu     -->
+    <nav class="menu">
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Produk</a>
+        </li>
+        <li>
+          <a href="">Lainnya</a>
+          <ul class="sub1">
+                    <li></li><a target="_blank" href="https://wa.me/+6285877079598">Kontak</a></li>
+                    <li><a href="">Tentang Kami</a></li>
+                </ul>
+            </li>
+      </ul>
+    </nav>
+    <!-- akhir bagian menu -->
+
+    <!-- bagian sidebar website -->
+    <aside class="sidebar">
+      <div class="widget">
+        <h2>Luigis'Design</h2>
+        <p>Luigi's desain adalah sebuah startup jasa desain yang melayani segala kebutuhan seperti Branding,Digital Marketing, Fotografi dan desain Grafis seperti Desain Poster,Logo, Company Profil,Brosur dll. Keunggulan : 
+        ✅harga ekonomis dan nggak bikin kantong tipis✅layanan sesuai konsep
+        ✅mengutamakan kepuasan pelanggan.
+        GARANSI UANG KEMBALI!!!, Luigi's Desain solusi segala kebutuhan desain anda.</p>
+      </div>
+      <div class="widget">
+        <h2>Instagram</h2>
+        <p>Teman-teman juga bisa melihat produk produk kami di sini <a target="_blank" href="https://www.instagram.com/luigis_desain/">www.instagram.com</a>.</p>
+      </div>
+    </aside>
+    <!-- akhir bagian sidebar website -->
+
+    <!-- bagian konten Blog -->
+    <div class="blog">
+      <div class="conteudo">
+        <div class="post-info">
+          Di Posting Oleh <b>Luigi's Design</b>
+        </div>
+        <img src="Undangan.jpg">
+        <h1> Contoh Desain yang kami Tawarkan </h1>
+        <hr>
+        <p>
+         bla bla bla
+        </p>        
+        <a href="#" class="continue-lendo">Selengkapnya →</a>
+      </div>
+      <div class="conteudo">
+        <div class="post-info">
+          Di Posting Oleh <b>Luigi's Design</b>
+        </div>
+        <img src="und.png">
+        <h1> Contoh Desain yang kami Tawarkan </h1>
+        <hr>
+        <p>
+          bla bla bla
+        </p>        
+        <a href="#" class="continue-lendo">Selengkapnya →</a>
+      </div>
+      <div class="conteudo">
+        <div class="post-info">
+          Di Posting Oleh <b>Luigi's Design</b>
+        </div>
+        <img src="banner.png">
+        <h1> Contoh Desain yang kami Tawarkan </h1>
+        <hr>
+        <p>
+          bla bla bla
+        </p>        
+        <a href="#" class="continue-lendo">Selengkapnya →</a>
+      </div>
+      <div class="conteudo">
+        <div class="post-info">
+          Di Posting Oleh <b>Luigi's Design</b>
+        </div>
+        <img src="handuk.png">
+        <h1> Contoh Desain yang kami Tawarkan </h1>
+        <hr>
+        <p>
+          bla bla bla
+        </p>        
+        <a href="#" class="continue-lendo">Selengkapnya →</a>
+      </div>
+      <div class="conteudo">
+        <div class="post-info">
+          Di Posting Oleh <b>Luigi's Design</b>
+        </div>
+        <img src="post.png">
+        <h1> Contoh Desain yang kami Tawarkan </h1>
+        <hr>
+        <p>
+          bla bla bla
+        </p>        
+        <a href="#" class="continue-lendo">Selengkapnya →</a>
+      </div>
+      <div class="conteudo">
+        <div class="post-info">
+          Di Posting Oleh <b>Luigi's Design</b>
+        </div>
+        <img src="kal.png">
+        <h1> Contoh Desain yang kami Tawarkan </h1>
+        <hr>
+        <p>
+          bla bla bla
+        </p>        
+        <a href="#" class="continue-lendo">Selengkapnya →</a>
+      </div>
+    </div>
+    <!-- akhir bagian konten Blog -->
+  </div>
+
+</body>
+</html>
